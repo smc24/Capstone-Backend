cb642d83b7838bb600e6490baaacfdfee2003300
unknown <sasham_c@yahoo.com>
2022-02-23 17:49:44 -0600

    created therapist controller, service and repository

---
+++ keep-it-real/src/main/java/com/keepitreal/keepitreal/controller/TherapistController.java
@@ -0,0 +1,14 @@
+package com.keepitreal.keepitreal.controller;
+
+import com.keepitreal.keepitreal.service.TherapistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api")
+public class TherapistController {
+
+    @Autowired
+    private TherapistService therapistService;
+}
---
+++ keep-it-real/src/main/java/com/keepitreal/keepitreal/repository/TherapistRepository.java
@@ -0,0 +1,7 @@
+package com.keepitreal.keepitreal.repository;
+
+import com.keepitreal.keepitreal.model.Therapist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TherapistRepository extends JpaRepository<Therapist, Long> {
+}
