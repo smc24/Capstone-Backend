76aa5b4037601afdb6f54b81590ddf733bbf6bfb
unknown <sasham_c@yahoo.com>
2022-02-23 21:29:17 -0600

    conflict and not found exceptions added

--- keep-it-real/src/main/java/com/keepitreal/keepitreal/controller/TherapistController.java
+++ keep-it-real/src/main/java/com/keepitreal/keepitreal/controller/TherapistController.java
@@ -1,11 +1,14 @@
 package com.keepitreal.keepitreal.controller;
 
+import com.keepitreal.keepitreal.model.Therapist;
 import com.keepitreal.keepitreal.service.TherapistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/api")
@@ -17,4 +20,17 @@ public class TherapistController {
     //checking connection with postman
     @GetMapping("/hello")
     public String getHelloWorld(){return "hello world";}
+
+    @GetMapping("/therapists/")
+    public List<Therapist> getAllTherapists(){return therapistService.getAllTherapists;}
+
+
+
+
+
+
+
+
+
 }
+
--- keep-it-real/src/main/java/com/keepitreal/keepitreal/service/TherapistService.java
+++ keep-it-real/src/main/java/com/keepitreal/keepitreal/service/TherapistService.java
@@ -1,12 +1,28 @@
 package com.keepitreal.keepitreal.service;
 
+import com.keepitreal.keepitreal.model.Therapist;
 import com.keepitreal.keepitreal.repository.TherapistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TherapistService {
 
     @Autowired
     private TherapistRepository therapistRepository;
+
+    //GETTING THE LIST OF THERAPISTS
+    public List<Therapist> getAllTherapists(){
+        List<Therapist> therapist = therapistRepository.findAll();
+        if(therapist==null){
+            
+        }
+    }
+
+
+
+
+
 }
